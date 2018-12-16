252207575defe1fd9d6228ad41df87374d909b26
Wesley Kenji <wkenji.oyama@gmail.com>
2018-12-16 14:02:07 -0200

    mais informacoes para logar

--- project.py
+++ project.py
@@ -19,7 +19,8 @@ session = DBSession()
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
