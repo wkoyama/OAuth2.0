be110cec6c2e831962b8be76d37cdc30e523c97f
Wesley Kenji <wkenji.oyama@gmail.com>
2018-12-16 14:43:15 -0200

    caso usuario nao exista crie um novo

--- project.py
+++ project.py
@@ -114,6 +114,10 @@ def gconnect():
     login_session['email'] = data['email']
 
     # See if a user exists, if it doesn't make a new one
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
 
     output = ''
     output += '<h1>Welcome, '
